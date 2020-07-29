b18b7721e3889c0180b733fd8d1fa80cb7c8643a
denstojkov <denstojkov@hotmail.com>
2020-07-29 13:07:18 +0000

    UX adj

--- public/assets/js/style.js
+++ public/assets/js/style.js
@@ -1,4 +1,14 @@
 $(function () {
     $('[data-toggle="popover"]').popover()
   })
-  
+
+let colors= ["#227CF2","#2793F2","#165FF2"];
+
+let gridColors = document.querySelectorAll(".mainGrid");
+
+
+for(let i=0; i <= gridColors.length; i++){
+	let random = Math.floor( Math.random() * Math.floor(2));
+	gridColors[i].style.backgroundColor = colors[random];
+	console.log(i);
+}
