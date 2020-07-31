5aa7f06795db51d97c49e5b5d13171d06aec0fef
denstojkov <denstojkov@hotmail.com>
2020-07-31 13:41:36 +0000

    Small changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ ejs = require("ejs"),
 methodOvveride = require("method-override");
 const index = require("./routes/index");
 const webshop = require("./routes/webshop");
+const userCP = require("./routes/userCP")
 router = express.Router();
 const fetch = require('node-fetch');
 
@@ -13,6 +14,7 @@ const fetch = require('node-fetch');
 app.use(express.static(__dirname + "/public/"));
 app.use("/index", index);
 app.use("/webshop", webshop);
+app.use("/userCP", userCP);
 app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/myapp", {
@@ -25,9 +27,6 @@ app.get("/", (req,res) => {
     res.redirect("/index");
 })
 
-app.get("/", (req,res) => {
-    res.redirect("/webshop");
-})
 // TEST
 
 
---
+++ routes/userCP.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { model } = require("mongoose");
+const { request } = require("express");
+let router = express.Router();
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('560993ce6d0c4888afea6c6f59739272');
+const fetch = require('node-fetch');
+
+
+
+
+
+router.get("/",(req,res) => {
+	res.render("userCP");
+})
+
+module.exports = router;
