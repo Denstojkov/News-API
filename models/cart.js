63fa43c92f2a4aeb3a139d77a297026b8e3f9292
denstojkov <denstojkov@hotmail.com>
2020-08-18 20:45:20 +0200

    small fixes

--- models/cart.js
+++ models/cart.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
  
 const Cart = new Schema({
-    product: [{ type: Schema.Types.ObjectId, ref: 'Story' }],
+    product: [{ type: Schema.Types.ObjectId, ref: 'Products' }],
     qty: Number
 
 });
